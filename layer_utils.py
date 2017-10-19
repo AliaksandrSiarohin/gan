c3eab1eb5550437b990aa563d015a7a7584e03a0
aliaksandr siarohin <aliaksandr.siarohin@gmail.com>
2017-10-20 00:11:32 +0300

    Remove LayerNorm, replace by InstanceNormalization.

--- layer_utils.py
+++ layer_utils.py
@@ -3,9 +3,6 @@ from keras.layers.convolutional import Conv2D, UpSampling2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.merge import Add
 from keras.layers.pooling import AveragePooling2D
-from keras import backend as K
-from keras.engine.topology import Layer
-from keras import initializers
 from keras.backend import tf as ktf
 
 
@@ -25,40 +22,7 @@ def jacobian(y_flat, x):
     return jacobian.stack()
 
 
-class LayerNorm(Layer):
-    def __init__(self, epsilon=1e-7, beta_init='zero', gamma_init='one', **kwargs):
-        self.beta_init = initializers.get(beta_init)
-        self.gamma_init = initializers.get(gamma_init)
-        
-        self.epsilon = epsilon
-        self.uses_learning_phase = True
-        super(LayerNorm, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        shape = (input_shape[-1],)
-        
-        self.gamma = K.variable(self.gamma_init(shape), name='{}_gamma'.format(self.name))
-        self.beta = K.variable(self.beta_init(shape), name='{}_beta'.format(self.name))
-        
-        self.trainable_weights = [self.gamma, self.beta]
-
-    def call(self, x, mask=None):
-        m, std = ktf.nn.moments(x, axes = [1, 2, 3], keep_dims=True)
-        x_normed = (x - m) / (K.sqrt(std + self.epsilon))
-        out = self.gamma * x_normed + self.beta
-        return out
-    
-    def compute_output_shape(self, input_shape):
-        return input_shape
-    
-    def get_config(self):
-        config = {"epsilon": self.epsilon}
-        base_config = super(LayerNorm, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
-
-
-def resblock(x, kernel_size, resample, nfilters, norm = BatchNormalization):
+def resblock(x, kernel_size, resample, nfilters, norm=BatchNormalization):
     assert resample in ["UP", "SAME", "DOWN"]
 
     if resample == "UP":
@@ -97,7 +61,6 @@ def resblock(x, kernel_size, resample, nfilters, norm = BatchNormalization):
         shortcut = Conv2D(nfilters, kernel_size, kernel_initializer='he_uniform',
                           padding = 'same', use_bias = True) (shortcut)        
         
-        convpath = x
         convpath = norm() (x)
         convpath = Activation('relu') (convpath)
         convpath = Conv2D(nfilters, kernel_size, kernel_initializer='he_uniform',
